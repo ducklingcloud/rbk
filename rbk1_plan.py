7e929f0305fad35e63b8bbc5e18f9c68094488d2
nameten <kai_nan@yahoo.com>
2019-07-11 17:17:11 +0800

    rbk1_plan.py: reinforcement

--- rbk1_plan.py
+++ rbk1_plan.py
@@ -21,9 +21,15 @@ import os, sys
 import subprocess
 import re
 
-find_cmd_tmpl = ("find $destdir \( -type f -o -type d -empty \) -ls "
-                 "> rbk_all_raw.dat")
-sort_cmd      = "sort -u -k 11 -o rbk_all.dat rbk_all_raw.dat"
+fn_rbk_target      = "rbk_target.dat"
+fn_rbk_all         = "rbk_all.dat"
+fn_rbk_all_raw     = "rbk_all_raw.dat"
+fn_rbk_incremental = "rbk_incremental.dat"
+fn_rbk_deleted     = "rbk_deleted.dat"
+
+find_cmd_tmpl = "find $destdir \( -type f -o -type d -empty \) -ls >"+ \
+    fn_rbk_all_raw
+sort_cmd      = "sort -u -k 11 -o "+ fn_rbk_all +" "+ fn_rbk_all_raw
 destdir       = "/kd/dvlp/fes-scheduler-log-analyzing"
 segment_unit  = 10*1000*1000     # input_arg in MByte
 target        = "target"
@@ -69,23 +75,23 @@ def main():
     get_args()
 
     # Check existing and warn
-    if target != "continue" and os.path.isfile("rbk_target.dat"):
-        print("Something exists. Please remove rbk_target.dat and *.job "
-              "and try again if you want to start a new one.")
-        sys.exit(0)
-              
-    f = open("rbk_target.dat", "w")
-    f.write(target +"\n")
-    f.close()
+    if os.path.isfile(fn_rbk_target):
+        if target != "continue":
+            print("Something exists. Please remove "+ fn_rbk_target +" and *.job and try again if you want to start a new one.")
+            sys.exit(0)
+    else:
+        f = open(fn_rbk_target, "w")
+        f.write(target +"\n")
+        f.close()
 
-    if not os.path.isfile("rbk_all_raw.dat"):
+    if target != "continue" or not os.path.isfile(fn_rbk_all_raw):
         find_cmd = find_cmd_tmpl.replace("$destdir", destdir)
         res = subprocess.run(find_cmd, shell=True, stderr=subprocess.PIPE)
         # -> rbk_all_raw.dat
         if len(res.stderr) > 0:
             print(res.stderr)
             sys.exit(1)
-    if not os.path.isfile("rbk_all.dat"):
+    if target != "continue" or not os.path.isfile(fn_rbk_all):
         res = subprocess.run(sort_cmd, shell=True, stderr=subprocess.PIPE)
         # -> rbk_all.dat
         if len(res.stderr) > 0:
@@ -93,28 +99,30 @@ def main():
             sys.exit(1)
 
     if last_rbk_all != "":
-        file_incremental = open("rbk_incremental.dat", "w")
-        file_deleted = open("rbk_deleted.dat", "w")
+        file_incremental = open(fn_rbk_incremental, "w")
+        file_deleted = open(fn_rbk_deleted, "w")
         with open(last_rbk_all) as f_last:
-            with open("rbk_all.dat") as f_now:
+            with open(fn_rbk_all) as f_now:
                 l_last = f_last.readline()
                 l_now  = f_now.readline()
-                path_last = path_now = ""
+                flag_last = flag_now = True
                 while l_last != "" and l_now != "":
-                    if path_last == "":
+                    if flag_last:
                         fields_last = l_last.split(maxsplit=10)
                         path_last = fields_last[10]
-                    if path_now == "":
+                        flag_last = False
+                    if flag_now:
                         fields_now  = l_now.split(maxsplit=10)
                         path_now = fields_now[10]
+                        flag_now = False
                     if path_last < path_now:
                         file_deleted.write(l_last)
                         l_last = f_last.readline()
-                        path_last = ""
+                        flag_last = True 
                     elif path_last > path_now:
                         file_incremental.write(l_now)
                         l_now = f_now.readline()
-                        path_now = ""
+                        flag_now = True
                     else:
                         if l_last != l_now and \
                            ("".join(fields_last[0:9]) != \
@@ -124,7 +132,7 @@ def main():
                             file_incremental.write(l_now)
                         l_last = f_last.readline()
                         l_now  = f_now.readline()
-                        path_last = path_now = ""
+                        flag_last = flag_now = True
 
                 while l_last != "":
                     file_deleted.write(l_last)
@@ -135,11 +143,11 @@ def main():
 
         file_deleted.close()
         file_incremental.close()
-        segment("rbk_incremental.dat")
+        segment(fn_rbk_incremental)
     else:
-        segment("rbk_all.dat")
+        segment(fn_rbk_all)
 
-    
+
 # main()
 
 if __name__ == "__main__":
